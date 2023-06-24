32d971bed8f638a0ab6ea898e68aaef0d2d60f7a
Harini chamathka <harinichamathka14@gmail.com>
2023-06-24 13:26:02 +0530

    Update all files

---
+++ Reviewer/comment.java
@@ -0,0 +1,36 @@
+package com.Reviewer;
+
+public class comment {
+	 private int id;
+	 private String title;
+	 private String description;
+	 
+	public comment(int id, String title, String description) {
+
+
+		this.id = id;
+		this.title = title;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public String gettitle() {
+		return title;
+	}
+
+	
+
+	public String getdescription() {
+		return description;
+	}
+
+	
+	 
+	 
+
+}
